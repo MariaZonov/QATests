efd5bc67b816e8112dd27fc20ad8005432f0379e
Vladimir Pershukov <vpershukov@B.local>
2025-03-09 11:54:17 +0300

    #3 Обновлены файл conftest и клиенты для работы с Breweries API

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from storage.credentials import CatApiUser
 
+
 @pytest.fixture(scope="session")
 def cats_api_auth():
-    return {"X-API-KEY" : CatApiUser.X_API_KEY}
+    return {"X-API-KEY": CatApiUser.X_API_KEY}

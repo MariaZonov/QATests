d6e4f9a1200d696a936774a291ff0ee6416c350b
Vladimir Pershukov <vpershukov@B.local>
2025-03-09 11:21:57 +0300

    #3 Добавлены комментарии к файлам

--- storage/urls.py
+++ storage/urls.py
@@ -1,10 +1,16 @@
-class BreweriesURLS:
+"""
+Файл с адресами вэб-сервисов и прочими URLs
+"""
+
+
+class BreweriesUrls:
     BASE_URL = "https://api.openbrewerydb.org/v1/breweries"
     URL_PAGE = BASE_URL + "?per_page={0}"
     URL_PAGE_BREWERIES = BASE_URL + "?page ={0}&per_page={1}"
     URL_TYPE = BASE_URL + "?by_type=micro&per_page={0}"
 
-class CatsApi:
+
+class CatsApiUrls:
     BASE_URL = "https://api.thecatapi.com"
     IMAGES_URL = BASE_URL + "/v1/images/search"
     FAVORITIES_URL = BASE_URL + "/v1/favourites"
